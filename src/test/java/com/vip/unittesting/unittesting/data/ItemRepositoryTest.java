b912b27b48b7a90f7691cad642e7aa3b4218428f
Avinash Oguri <avinashoguris@gmail.com>
2023-02-06 19:48:42 +0530

    all layesrs test covered.

--- src/test/java/com/vip/unittesting/unittesting/data/ItemRepositoryTest.java
+++ src/test/java/com/vip/unittesting/unittesting/data/ItemRepositoryTest.java
@@ -5,16 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.vip.unittesting.unittesting.data.ItemRepository;
 import com.vip.unittesting.unittesting.model.Item;
 
 
